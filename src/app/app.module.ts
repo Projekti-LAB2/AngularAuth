ed3f006ff5d7c30fa13d4f2a34ef567175a951b1
DrenVisoka <dv51406@ubt-uni.net>
2023-05-05 11:51:15 +0200

    adding style on login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { MatFormFieldModule, MatIconModule, MatInputModule } from '@angular/material';
+import {  MatButtonModule, MatIconModule, MatInputModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -17,13 +18,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
     ReactiveFormsModule,
     MatIconModule,
-    BrowserAnimationsModule
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
 
+  formGroup = new FormGroup({
+    email: new FormControl(``, [Validators.required, Validators.email]),
+    password: new FormControl<string | null>(``, [Validators.required, Validators.minLength(6)])
+  })
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  hasError(formControlName:string, error:string) {
+    if(formControlName === 'email'){
+      return this.formGroup.controls['email'].hasError(error);
+    }else{
+      return this.formGroup.controls['password'].hasError(error);
+    }
+  }
 }

e1ea1b5fb5fab9ae18c7278c47b3520242bdf3dc
DrenVisoka <dv51406@ubt-uni.net>
2023-06-20 21:40:27 +0200

    fixing filter on tickets

--- src/app/components/tickets/tickets.component.ts
+++ src/app/components/tickets/tickets.component.ts
@@ -12,69 +12,168 @@ import { StartPoint } from 'src/app/models/startPoint';
 @Component({
   selector: 'app-tickets',
   templateUrl: './tickets.component.html',
-  styleUrls: ['./tickets.component.css']
+  styleUrls: ['./tickets.component.css'],
 })
 export class TicketsComponent implements OnInit {
   tickets = new BehaviorSubject<Ticket[]>([]);
   cities: City[] = [];
   startPoints: StartPoint[] = [];
-  displayedColumns = ['TicketId','TicketNumber','Price','StartPoint','City','Time','Date', 'edit','delete'];
+  displayedColumns = [
+    'TicketId',
+    'TicketNumber',
+    'Price',
+    'StartPoint',
+    'City',
+    'Time',
+    'Date',
+    'edit',
+    'delete',
+  ];
+  options = [
+    'TicketId',
+    'TicketNumber',
+    'Price',
+    'StartPoint',
+    'City',
+    'Time',
+    'Date',
+  ];
 
-  constructor(private ticketService: TicketService,private matDialog: MatDialog,
-    private cityService: CityService, private startPointService: StartPointService) { }
+  countNumber = 0;
+  columnData: string = '';
+  allTicketValues: Ticket[] = [];
+  searchText: string = '';
+
+  constructor(
+    private ticketService: TicketService,
+    private matDialog: MatDialog,
+    private cityService: CityService,
+    private startPointService: StartPointService
+  ) {}
 
   ngOnInit(): void {
-    this.ticketService.getTickets().subscribe(res => {
+    this.ticketService.getTickets().subscribe((res) => {
       this.tickets.next(res);
+      this.allTicketValues = res;
     });
-    this.cityService.getCities().subscribe(res => {
+    this.cityService.getCities().subscribe((res) => {
       this.cities = res;
     });
-    this.startPointService.getStartPoints().subscribe(res => {
+    this.startPointService.getStartPoints().subscribe((res) => {
       this.startPoints = res;
     });
   }
 
-
   AddTicket() {
     const dialog = this.matDialog.open(CreateTicketComponent, {
       width: `90%`,
-      data: {cities: this.cities, startPoints: this.startPoints}
-    })
-    dialog.afterClosed().subscribe((res:any) => {
-      this.ticketService.createTicket(res).subscribe(res => {
-          this.tickets.value.push(res);
-          this.tickets.next(this.tickets.value)
+      data: { cities: this.cities, startPoints: this.startPoints },
+    });
+    dialog.afterClosed().subscribe((res: any) => {
+      this.ticketService.createTicket(res).subscribe((res) => {
+        this.tickets.value.push(res);
+        this.tickets.next(this.tickets.value);
       });
-    })
+    });
   }
-  deleteTicket(id:string) {
+  deleteTicket(id: string) {
     this.ticketService.deleteTicket(id).subscribe(() => {
-      const index = this.tickets.value.findIndex(user => user.TicketId.toString() === id.toString());
-        if(index !== -1) {
-          this.tickets.value.splice(index,1);
-          this.tickets.next(this.tickets.value);
-        }
-    })
+      const index = this.tickets.value.findIndex(
+        (user) => user.TicketId.toString() === id.toString()
+      );
+      if (index !== -1) {
+        this.tickets.value.splice(index, 1);
+        this.tickets.next(this.tickets.value);
+      }
+    });
   }
-  editTicket(ticket:Ticket) {
+  editTicket(ticket: Ticket) {
     const dialog = this.matDialog.open(CreateTicketComponent, {
       width: `90%`,
-      data: {ticket: ticket, isEditMode:true, cities: this.cities, startPoints: this.startPoints}
-    })
-    dialog.afterClosed().subscribe((res:any) => {
-      this.ticketService.updateTicket(ticket.TicketId.toString(),res).subscribe(res => {
-        const index = this.tickets.value.findIndex(user => user.TicketId === res.TicketId);
-        if(index !== -1){
-          this.tickets.value.splice(index,1,res);
-          this.tickets.next(this.tickets.value);
-        }
-      });
-    })
+      data: {
+        ticket: ticket,
+        isEditMode: true,
+        cities: this.cities,
+        startPoints: this.startPoints,
+      },
+    });
+    dialog.afterClosed().subscribe((res: any) => {
+      this.ticketService
+        .updateTicket(ticket.TicketId.toString(), res)
+        .subscribe((res) => {
+          const index = this.tickets.value.findIndex(
+            (user) => user.TicketId === res.TicketId
+          );
+          if (index !== -1) {
+            this.tickets.value.splice(index, 1, res);
+            this.tickets.next(this.tickets.value);
+          }
+        });
+    });
   }
-  getStartPoint(id:number | null) {
-    return id !== null ? this.startPoints.find(item => 
-      item.StartPointId?.toString() === id.toString())?.DeparatureCityName : '';
+  getStartPoint(id: number | null) {
+    return id !== null
+      ? this.startPoints.find(
+          (item) => item.StartPointId?.toString() === id.toString()
+        )?.DeparatureCityName
+      : '';
+  }
+
+  sort(column: any) {
+    this.countNumber++;
+    if (this.columnData !== column) {
+      this.columnData = column;
+      this.countNumber = 1;
+    }
+    if (this.countNumber === 1) {
+      this.columnData = column;
+      this.tickets.value.sort((a: any, b: any) =>
+        a[column] < b[column] === true
+          ? -1
+          : (a[column] === b[column]) === true
+          ? 0
+          : 1
+      );
+      this.tickets.next(this.tickets.value);
+    } else if (this.countNumber === 2) {
+      this.columnData = column;
+
+      this.tickets.value
+        .sort((a: any, b: any) =>
+          a[column] < b[column] === true
+            ? -1
+            : (a[column] === b[column]) === true
+            ? 0
+            : 1
+        )
+        .reverse();
+      this.tickets.next(this.tickets.value);
+    } else {
+      this.countNumber = 0;
+    }
+  }
+  filterCities() {
+    if (this.filterByColumn) {
+      const filteredValues = this.allTicketValues.filter((ticket) =>
+      {const ticketData = this.filterByColumn === 'City' ?
+          (ticket[this.filterByColumn as keyof Ticket] as City).CityName : (
+            this.filterByColumn === 'StartPoint' ? 
+              (ticket[this.filterByColumn as keyof Ticket] as StartPoint)?.DeparatureCityName : ticket[this.filterByColumn as keyof Ticket])
+        return ticketData.toString().toLowerCase().includes(this.searchText.toString().toLowerCase())}
+      );
+      this.tickets.next(filteredValues);
+    } else {
+      const filteredValues = this.allTicketValues.filter((ticket) =>
+        ticket.TicketId.toString()
+          .toLowerCase()
+          .includes(this.searchText.toString().toLowerCase())
+      );
+      this.tickets.next(filteredValues);
+    }
+  }
+  filterByColumn: string = '';
+  changeValue(option: string) {
+    this.filterByColumn = option;
   }
 }
  
